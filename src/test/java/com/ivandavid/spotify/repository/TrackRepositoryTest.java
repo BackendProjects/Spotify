9ae557c73123cd7790a0caee13cb96c65b65353b
ivandavid858 <ivandavid858@gmail.com>
2023-01-28 20:05:44 +0100

    Add tracks with genres

--- src/test/java/com/ivandavid/spotify/repository/TrackRepositoryTest.java
+++ src/test/java/com/ivandavid/spotify/repository/TrackRepositoryTest.java
@@ -27,26 +27,41 @@ class TrackRepositoryTest {
         trackRepository.deleteAll();
         var genre1 = genreRepository.findById(1L).get();
         var genre2 = genreRepository.findById(2L).get();
+        var genre3 = genreRepository.findById(3L).get();
+        var genre4 = genreRepository.findById(4L).get();
+        var genre5 = genreRepository.findById(5L).get();
+        var genre6 = genreRepository.findById(6L).get();
+        var genre7 = genreRepository.findById(7L).get();
+        var genre8 = genreRepository.findById(8L).get();
+
         var genresForTrack1 = new ArrayList<Genre>();
         genresForTrack1.add(genre1);
-        genresForTrack1.add(genre2);
+        genresForTrack1.add(genre4);
+        genresForTrack1.add(genre8);
 
-        var genre3 = genreRepository.findById(3L).get();
-        var genre5 = genreRepository.findById(5L).get();
         var genresForTrack2 = new ArrayList<Genre>();
+        genresForTrack2.add(genre2);
         genresForTrack2.add(genre3);
+        genresForTrack2.add(genre4);
         genresForTrack2.add(genre5);
-
-        var genre6 = genreRepository.findById(7L).get();
-        var genre7 = genreRepository.findById(9L).get();
-        var genresForTrack3 = new ArrayList<Genre>();
         genresForTrack2.add(genre6);
-        genresForTrack2.add(genre7);
+
+        var genresForTrack3 = new ArrayList<Genre>();
+        genresForTrack3.add(genre3);
+        genresForTrack3.add(genre7);
+        genresForTrack3.add(genre8);
+
+        var genresForTrack4 = new ArrayList<Genre>();
+        genresForTrack3.add(genre2);
+        genresForTrack3.add(genre4);
+        genresForTrack3.add(genre6);
+        genresForTrack3.add(genre8);
 
         List<Track> tracks = List.of(
-                new Track("song1", 400L, genresForTrack1),
-                new Track("song2", 410L, genresForTrack2),
-                new Track("song3", 410L, genresForTrack3)
+                new Track("track1", 400L, genresForTrack1),
+                new Track("track2", 410L, genresForTrack2),
+                new Track("track3", 420L, genresForTrack3),
+                new Track("track4", 430L, genresForTrack4)
         );
         trackRepository.saveAll(tracks);
     }
@@ -54,7 +69,7 @@ class TrackRepositoryTest {
     @Test
     void test_prueba() {
         var numTracks = trackRepository.findAll().size();
-        Assertions.assertEquals(3, numTracks);
+        Assertions.assertEquals(4, numTracks);
     }
 
     @AfterEach
