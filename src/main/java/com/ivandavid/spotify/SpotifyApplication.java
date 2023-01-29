931d8219595af691514c111191d59647477f8772
ivandavid858 <ivandavid858@gmail.com>
2023-01-29 15:56:40 +0100

    Delete unnecessary comments

--- src/main/java/com/ivandavid/spotify/SpotifyApplication.java
+++ src/main/java/com/ivandavid/spotify/SpotifyApplication.java
@@ -11,37 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 @SpringBootApplication
-public class SpotifyApplication /*implements CommandLineRunner*/ {
-
-    /*private final TrackRepository trackRepository;
-    private final GenreRepository genreRepository;
-
-    public SpotifyApplication(TrackRepository trackRepository, GenreRepository genreRepository) {
-        this.trackRepository = trackRepository;
-        this.genreRepository = genreRepository;
-    }*/
+public class SpotifyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpotifyApplication.class, args);
     }
-
-    /*@Override
-    public void run(String... args) throws Exception {
-
-
-
-
-
-        trackRepository.deleteAll();
-        var tracks = List.of(
-                new Track("song1", 400L),
-                new Track("song2", 410L),
-                new Track("song3", 420L)
-        );
-        trackRepository.saveAll(tracks);
-
-        var genre = new Genre("genre1", 2L);
-        genreRepository.save(genre);
-
-    }*/
 }
